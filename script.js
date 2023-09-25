efafa9c8dfcf7c04a8f9a8f577ee09f47a466552
Dev1908 <144559846+Dev1908@users.noreply.github.com>
2023-09-25 10:53:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// Get references to the button and hidden text
+const toggleButton = document.getElementById("toggleButton");
+const hiddenText = document.getElementById("hiddenText");
+
+// Add a click event listener to the button
+toggleButton.addEventListener("click", () => {
+    // Toggle the visibility of the text
+    if (hiddenText.style.display === "none") {
+        hiddenText.style.display = "block";
+    } else {
+        hiddenText.style.display = "none";
+    }
+});
+
+const toggleButton1 = document.getElementById("toggleButton1");
+const hiddenText1 = document.getElementById("hiddenText1");
+
+// Add a click event listener to the button
+toggleButton1.addEventListener("click", () => {
+    // Toggle the visibility of the text
+    if (hiddenText1.style.display === "none") {
+        hiddenText1.style.display = "block";
+    } else {
+        hiddenText1.style.display = "none";
+    }
+});
+
+const toggleButton2 = document.getElementById("toggleButton2");
+const hiddenText2 = document.getElementById("hiddenText2");
+
+// Add a click event listener to the button
+toggleButton2.addEventListener("click", () => {
+    // Toggle the visibility of the text
+    if (hiddenText2.style.display === "none") {
+        hiddenText2.style.display = "block";
+    } else {
+        hiddenText2.style.display = "none";
+    }
+});
+
+const toggleButton3 = document.getElementById("toggleButton3");
+const hiddenText3 = document.getElementById("hiddenText3");
+
+// Add a click event listener to the button
+toggleButton3.addEventListener("click", () => {
+    // Toggle the visibility of the text
+    if (hiddenText3.style.display === "none") {
+        hiddenText3.style.display = "block";
+    } else {
+        hiddenText3.style.display = "none";
+    }
+});
+
+const toggleButton4 = document.getElementById("toggleButton4");
+const hiddenText4 = document.getElementById("hiddenText4");
+
+// Add a click event listener to the button
+toggleButton4.addEventListener("click", () => {
+    // Toggle the visibility of the text
+    if (hiddenText4.style.display === "none") {
+        hiddenText4.style.display = "block";
+    } else {
+        hiddenText4.style.display = "none";
+    }
+});
